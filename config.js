f8008fd025a06d9121c8dbff5c27b00aa469a9f5
Nikola Novakovic <nikola.novakovic9@gmail.com>
2015-06-05 15:22:00 +0200

    Init commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'port': process.env.PORT || 8080,
+  'database': 'mongodb://nik:loptamasna23@proximus.modulusmongo.net:27017/xub5Usoj',
+  'secret': 'ilovescotchscotchyscotchscotch'
+};
